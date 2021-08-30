38c549f1eb0491ffe2d2547ee0dd0ecdf357a17a
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-08-30 20:34:25 -0300

    feat: finishing changes

---
+++ src/components/Comments/index.tsx
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+
+export default class Comments extends Component {
+  componentDidMount() {
+    const script = document.createElement('script');
+    const anchor = document.getElementById('inject-comments-for-uterances');
+    script.setAttribute('src', 'https://utteranc.es/client.js');
+    script.setAttribute('crossorigin', 'anonymous');
+    script.setAttribute('async', 'true');
+    script.setAttribute('repo', 'GBDev13/space-traveling');
+    script.setAttribute('issue-term', 'pathname');
+    script.setAttribute('theme', 'dark-blue');
+    anchor.appendChild(script);
+  }
+
+  render() {
+    return (
+      <div
+        id="inject-comments-for-uterances"
+        style={{ marginBottom: '4rem' }}
+      />
+    );
+  }
+}
